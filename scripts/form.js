97590b3d4fda58ee6f344fb13a9407c61184f61b
achan17 <anthonychietto@gmail.com>
2023-11-29 04:35:20 -0800

    safety submission

--- scripts/form.js
+++ scripts/form.js
@@ -8,6 +8,16 @@ function loadHandler() {
         mode.className='light';
     }
     darkBtn.addEventListener('click', btnPressed);
+    addEventListener()
+
+    let nameInput = document.getElementById('name');
+    nameInput.addEventListener('input', nameValidity);
+    let emailInput = document.getElementById('email');
+    emailInput.addEventListener('input', emailValidity);
+    let commentInput = document.getElementById('comments');
+    commentInput.addEventListener('input', commentValidity);
+    commentInput.addEventListener('change', commentMisinput);
+    
 
 }
 
@@ -20,6 +30,68 @@ function btnPressed() {
     else {
         darkBtn.className='dark';
         localStorage.setItem('darkLight', 'dark');
+    }
+}
+
+function nameValidity() {
+    let nameInput = document.getElementById('name');
+    if(nameInput.validity.valueMissing) {
+        nameInput.setCustomValidity('A name is required to be entered!');
+    } else {
+        nameInput.setCustomValidity('');
+    }
+}
+
+function emailValidity() {
+    let emailInput = document.getElementById('email');
+    if(emailInput.validity.typeMismatch) {
+        emailInput.setCustomValidity('Please enter a valid email address :)');
+    } else {
+        emailInput.setCustomValidity('');
+    }
+}
+
+function commentValidity() {
+    let commentInput = document.getElementById('comments');
+    if(commentInput.validity.tooShort) {
+        commentInput.setCustomValidity('Please enter at least one more word into the comments, thank you :)');
+    }
+    else if(commentInput.validity.tooLong) {
+        commentInput.setCustomValidity('Your comment is a little bit too lengthy, please try to shorten, thank you :)');
+    }
+    else {
+        commentInput.setCustomValidity('');
+    }
+}
+
+function hideAlert() {
+    let errorAlert = document.getElementById('message');
+    let errorMsg = document.getElementById('error_comments');
+    errorAlert.style.setProperty('display', 'none');
+    errorMsg.style.setProperty('display', 'inline');
+
+}
+
+function commentMisinput() {
+    console.log('Change occured');
+    let commentInput = document.getElementById('comments');
+    let errorMsg = document.getElementById('error_comments');
+    let errorAlert = document.getElementById('message');
+    console.log(commentInput.value);
+    let emojiRegex = /\p{Emoji}/u;
+    if(emojiRegex.test(commentInput.value)){
+        commentInput.className='incorrect';
+        commentInput.setCustomValidity('Unfortunately emojis are not allowed in the comments sorry');
+        errorMsg.style.setProperty('display', 'none');
+        errorMsg.innerText=('Unfortunately emojis are not allowed in the comments sorry');
+        errorAlert.innerText=('Unfortunately emojis are not allowed in the comments sorry');
+        errorAlert.style.setProperty('display', 'block');
+        setTimeout(hideAlert, 3000);
+
+    }
+    else {
+        commentInput.className='correct';
+        errorMsg.innerText='';       
     }
 }
 
